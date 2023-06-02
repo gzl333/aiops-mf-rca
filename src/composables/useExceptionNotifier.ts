0015f79b92dafee1abd0557a9c9742bbf61f7517
mimas <zlguo@cnic.cn>
2023-06-02 15:47:17 +0800

    feat: 增加version.md文件

--- src/composables/useExceptionNotifier.ts
+++ src/composables/useExceptionNotifier.ts
@@ -6,7 +6,8 @@ export default function () {
   return (exception: unknown, source?: string) => {
     // axios error
     if (exception instanceof AxiosError) {
-      if (exception.response?.data) { // 业务错误
+      if (exception.response?.data) {
+        // 业务错误
         Notify.create({
           classes: 'notification-negative shadow-15',
           icon: 'mdi-alert',
