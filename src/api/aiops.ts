810a86fa4b4d781019fccf11415926a7e36834e8
mimas <zlguo@cnic.cn>
2023-06-02 09:52:13 +0800

    project init

---
+++ src/api/aiops.ts
@@ -0,0 +1,9 @@
+// aiops backend api
+
+/* eslint-disable camelcase */
+
+// import { axiosAiops } from 'boot/axios'
+
+export default {
+  //  apis
+}
---
+++ src/api/service.ts
@@ -0,0 +1,9 @@
+// service api
+
+/* eslint-disable camelcase */
+
+// import { axiosService } from 'boot/axios'
+
+export default {
+  // apis
+}
